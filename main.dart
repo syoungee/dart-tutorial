2dc894f659689911f3cf4ac90976e4d483705cbe
syoungee <liahwang@tecace.com>
2023-10-22 15:49:00 +0900

    1.2 Dynamic Type

--- main.dart
+++ main.dart
@@ -1,11 +1,15 @@
 void main(){
-  var name = 'sun young';
-  String name2 = 'sun young2';
+  // dynamic types
+  // DART는 개발자 친화적인 언어이다.
 
-  name = 'my name is sun young';
-  name2 = 'my name is sun young2';
+  var name; // dynamic type으로 지정된다.
+  name = 'sun young';
+  name = 22;
+  name = true;
 
-  // var로 타입을 지정해도 되고 string으로 타입을 지정해도 추후 데이터 수정이 가능하다.
-  print(name);
-  print(name2);
+
+  dynamic name2 = 'sun young2';
+  print(name); // true
+  print(name2); // sun young2
+
 }

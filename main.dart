fea26ac988c89ab73a20636f76dc23e3670f5363
syoungee <liahwang@tecace.com>
2023-10-22 15:44:08 +0900

    1.1 The Var Keyword

--- main.dart
+++ main.dart
@@ -1,3 +1,11 @@
 void main(){
-  print('hello world');
+  var name = 'sun young';
+  String name2 = 'sun young2';
+
+  name = 'my name is sun young';
+  name2 = 'my name is sun young2';
+
+  // var로 타입을 지정해도 되고 string으로 타입을 지정해도 추후 데이터 수정이 가능하다.
+  print(name);
+  print(name2);
 }

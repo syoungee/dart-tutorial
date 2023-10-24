8cf1405659eeb6140ca71661388d44ec8bab2481
sunyoung <sunyoungah69@gmail.com>
2023-10-24 10:31:31 +0900

    2.2 String Interpolation

--- main.dart
+++ main.dart
@@ -1,10 +1,13 @@
-
-void main(){
-  // Recap
-  // Dart의 스타일가이드 - var를 최대한 많이 쓰고 int등은 class의 property를 작성할 때 사용하는 게 권장된다.
-  int i = 12;
-  var name = 'la';
-  i = 12121212;
-  name = 'lalalalala';
-  // 메소드나 작은 함수 안에서 지역변수를 선언할 때는 var 사용 권장
+void main() {
+  var giveMeFive = true;
+  var numbers = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    if(giveMeFive) 5,
+  ];
+  print(numbers);
 }

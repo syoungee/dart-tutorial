718679705e8a38ff98945984879f15af3f5c92a4
sunyoung <sunyoungah69@gmail.com>
2023-10-26 14:51:28 +0900

    2.3 collection for

--- main.dart
+++ main.dart
@@ -1,9 +1,7 @@
 void main() {
-  // string interpolation
-  var name = 'sun young';
-  var age = 29;
-  var greeting = '''Hello everyone, my name is 
-  $name and I'm ${age - 1}''';
+  // collection for
+  var oldFriends = ['sunyoung', 'hwang', 'lia'];
+  var newFriends = ['a', 'b', 'c', for (var friend in oldFriends) "🩵 $friend"];
 
-  print(greeting);  
+  print(newFriends);
 }

8a1aa92be3fc67f2a8467824bf659f8a9902b681
sunyoung <sunyoungah69@gmail.com>
2023-10-26 15:39:23 +0900

    2.5 Sets

--- main.dart
+++ main.dart
@@ -1,20 +1,7 @@
 void main() {
-  // map은 js나 ts의 object, python의 dictionary
-  var player = {
-    'name': 'sun young',
-    'xp': 99.99,
-    'superpower': false,
-  };
-  // Type: Map<String, Object>
-  var player2 = {
-    'name': 'sun young2',
-    'xp': '99.99',
-    'superpower': 'false',
-  };
-  // Type: Map<String, String>
-
-  Map<List<int>, bool> player3 = {
-    [1, 2, 3, 5]: true,
-  };
-
+  List<int> numbers = [1, 2, 3, 4];
+  numbers.add(1);
+  numbers.add(1);
+  numbers.add(1);
+  print(numbers);
 }

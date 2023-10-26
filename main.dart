480f742ac1a43b8faa2e65fbe5017a412d1db1bc
sunyoung <sunyoungah69@gmail.com>
2023-10-26 16:52:24 +0900

    3.0 defining a function

--- main.dart
+++ main.dart
@@ -1,7 +1,8 @@
+String sayHello(String potato) => 'Hello $potato nice to meet you!';
+
+num plus(num a, num b) => a + b;
+
 void main() {
-  List<int> numbers = [1, 2, 3, 4];
-  numbers.add(1);
-  numbers.add(1);
-  numbers.add(1);
-  print(numbers);
+  print(sayHello('sssss'));
+  print(plus(999, 1));
 }
